16ea9ed2f53c44963f969a9a194bb3027192e03f
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-07-12 17:11:37 +0300

    Allow automatic piece stage cost insertion while blocking manual edits

--- wood_factory/wood_factory/doctype/factory_piece_stage_cost/factory_piece_stage_cost.py
+++ wood_factory/wood_factory/doctype/factory_piece_stage_cost/factory_piece_stage_cost.py
@@ -8,7 +8,9 @@ class FactoryPieceStageCost(Document):
             frappe.throw("Factory Piece Stage Cost records are created automatically")
 
     def on_update(self):
-        if not self.is_new() and not getattr(self.flags, "factory_cost_update", False):
+        automatic_insert = getattr(self.flags, "factory_cost_insert", False)
+        automatic_update = getattr(self.flags, "factory_cost_update", False)
+        if not self.is_new() and not automatic_insert and not automatic_update:
             frappe.throw("Factory Piece Stage Cost records cannot be edited manually")
 
     def on_trash(self):
