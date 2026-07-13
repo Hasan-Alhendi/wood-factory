e5d0587eca06d03e93976a3dbb8bdfe49cb7b4cb
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-07-13 10:02:51 +0300

    Use centralized financial report permissions

--- wood_factory/wood_factory/page/factory_reports/factory_reports.py
+++ wood_factory/wood_factory/page/factory_reports/factory_reports.py
@@ -1,15 +1,12 @@
 import frappe
 
 from wood_factory.reports import get_factory_report_data
-
-
-ALLOWED_ROLES = {"System Manager", "Manufacturing Manager", "Accounts Manager"}
+from wood_factory.security import ACCOUNTING_ROLES, MANAGEMENT_ROLES, require_any_role
 
 
 @frappe.whitelist()
 def get_reports(from_date=None, to_date=None, company=None, customer=None, status=None):
-    if not ALLOWED_ROLES.intersection(frappe.get_roles()):
-        frappe.throw("You are not permitted to view factory financial reports", frappe.PermissionError)
+    require_any_role(ACCOUNTING_ROLES | MANAGEMENT_ROLES, "You are not permitted to view factory financial reports")
     return get_factory_report_data(
         from_date=from_date,
         to_date=to_date,
