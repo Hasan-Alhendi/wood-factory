5c4adf02f4139e57b4c357ece2f4dfc8c72c2517
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-07-13 09:28:17 +0300

    Protect factory financial reports by role

--- wood_factory/wood_factory/page/factory_reports/factory_reports.py
+++ wood_factory/wood_factory/page/factory_reports/factory_reports.py
@@ -3,8 +3,13 @@ import frappe
 from wood_factory.reports import get_factory_report_data
 
 
+ALLOWED_ROLES = {"System Manager", "Manufacturing Manager", "Accounts Manager"}
+
+
 @frappe.whitelist()
 def get_reports(from_date=None, to_date=None, company=None, customer=None, status=None):
+    if not ALLOWED_ROLES.intersection(frappe.get_roles()):
+        frappe.throw("You are not permitted to view factory financial reports", frappe.PermissionError)
     return get_factory_report_data(
         from_date=from_date,
         to_date=to_date,
