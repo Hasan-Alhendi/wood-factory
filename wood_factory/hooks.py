cc26d4e3164b89dfd80db01e60c1b421d3c40555
Hasan Alhendi <72380459+Hasan-Alhendi@users.noreply.github.com>
2026-07-13 09:49:32 +0300

    Register factory role setup and permission hooks

--- wood_factory/hooks.py
+++ wood_factory/hooks.py
@@ -5,12 +5,29 @@ app_description = "Wood factory customizations"
 app_email = "eng.mohamad.hasan.alhendi@gmail.com"
 app_license = "mit"
 
+before_install = "wood_factory.setup.ensure_factory_roles"
+after_install = "wood_factory.setup.ensure_factory_roles"
+after_migrate = "wood_factory.setup.ensure_factory_roles"
+
 scheduler_events = {
     "cron": {
         "*/15 * * * *": [
             "wood_factory.alerts.evaluate_factory_alerts"
         ]
     }
+}
+
+permission_query_conditions = {
+    "Factory Order": "wood_factory.security.factory_order_query",
+    "Factory Piece": "wood_factory.security.factory_piece_query",
+    "Piece Exception": "wood_factory.security.piece_exception_query",
+    "Factory Alert Log": "wood_factory.security.factory_alert_query",
+}
+
+has_permission = {
+    "Factory Cost Ledger": "wood_factory.security.financial_document_permission",
+    "Factory Accounting Settings": "wood_factory.security.financial_document_permission",
+    "Factory Worker Cost Rate": "wood_factory.security.financial_document_permission",
 }
 
 doc_events = {
